83798912d85d5ed418ff17f5757e7b68fbc39108
GraphtyLove <maxim.berge@gmail.com>
2022-08-08 06:03:27 +0200

    Fist commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Http call performence
+This repo demonstrate how to reduce processing time of HTTP call in python.
+
+As the `requests` module won't allow full performances, I used the [httpx module](https://www.python-httpx.org/).
+
+This material is highly inspired on [this video](https://www.youtube.com/watch?v=qAh5dDODJ5k). I encourage your to see it for more explainations!
+
+## Installation
+To run this code you will need the `httpx` module.
+
+You can install it with:
+
+`pip install httpx`
+
+## Code
+You can find the here in [main.py](./main.py) with some comments.
+
+## Perf report
+Those are the result of GET requests made on `https://quotable.io/random` with a Macbook Pro 16" i9 processor on 08/08/2022.
+
+The results could change depending on the API but the gap should be more or less the same.
+
+### requests.get for loop
+- 50 -> 24 sec
+
+### Httpx.get for loop
+- 50 -> 24 sec
+
+### requests.Session.get SYNC for loop
+- 50 -> 6 sec
+- 150 -> 19 sec
+
+### Httpx.client.get SYNC for loop
+- 50 -> 6 sec
+- 150 -> 19 sec
+
+### Httpx.client.get ASYNC for loop
+- 50 -> 6 sec
+- 150 -> 19 sec
+
+### Httpx.client.get ASYNC tasks
+- 50 -> 6 sec
+- 150 -> 1.6 sec
+- 1000 -> 9 sec
---
+++ main.py
@@ -0,0 +1,104 @@
+import httpx
+import requests
+from time import perf_counter
+import asyncio
+
+URL = "https://quotable.io/random"
+
+def requests_for_loop(number_of_request: int = 50):
+    """Classic for loop with get request."""
+    start = perf_counter()
+
+    for i in range(number_of_request):
+        res = requests.get(URL)
+        print(i, res.json()["author"])
+
+    end = perf_counter()
+    print("Requests FOR LOOP -> Time: ", end - start) 
+
+
+def requests_for_loop_client_sync(number_of_request: int = 50):
+    """For loop with Session."""
+    start = perf_counter()
+    
+    with requests.Session() as session:
+        for i in range(number_of_request):
+            res = session.get(URL)
+            print(i, res.json()["author"])
+    
+    end = perf_counter()
+    print("FOR LOOP CLIENT -> Time: ", end - start)
+
+
+def httpx_for_loop(number_of_request: int = 50):
+    """Classic for loop with get request."""
+    start = perf_counter()
+
+    for i in range(number_of_request):
+        res = httpx.get(URL)
+        print(i, res.json()["author"])
+
+    end = perf_counter()
+    print("Httpx FOR LOOP -> Time: ", end - start)    
+
+
+def httpx_for_loop_client_sync(number_of_request: int = 50):
+    """For loop with Client session."""
+    start = perf_counter()
+    
+    with httpx.Client() as client:
+        for i in range(number_of_request):
+            res = client.get(URL)
+            print(i, res.json()["author"])
+    
+    end = perf_counter()
+    print("FOR LOOP CLIENT -> Time: ", end - start)
+
+
+async def for_loop_client_async(number_of_request: int = 50):
+    """For loop with async Client session."""
+    start = perf_counter()
+    
+    async with httpx.AsyncClient() as client:
+        for i in range(number_of_request):
+            res = await client.get(URL)
+            print(i, res.json()["author"])
+    
+    end = perf_counter()
+    print("FOR LOOP CLIENT -> Time: ", end - start )
+
+
+async def tasks_client_async(number_of_request: int = 50):
+    """
+    Asyncio tasks with AsyncClient.
+    A bit more complex to code but WAY more efficient!
+    
+    Also, this won't process the request in the correct order.
+    But it will sort it when finished.
+    """
+    start = perf_counter()
+    tasks = []
+
+    async with httpx.AsyncClient() as client:
+        for i in range(number_of_request):
+            tasks.append(asyncio.create_task(
+                # Can be replace by a custom ASYNC function
+                client.get(URL)
+            ))
+        
+        results = await asyncio.gather(*tasks)
+        
+        print("Number of requests done: ", len(results))
+    
+    end = perf_counter()
+    print("Tasks gather CLIENT -> Time: ", end - start )
+
+
+if __name__ == "__main__":
+    number_of_request = 1000
+    
+    # for_loop(number_of_request)
+    
+    # for_loop_client_sync(number_of_request)
+    #asyncio.run(for_loop_client_async(number_of_request))
+    asyncio.run(tasks_client_async(number_of_request))
